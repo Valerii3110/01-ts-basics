f44b8574aabc29fd7371307c9e07a2e007a9d870
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-08-23 08:27:10 +0300

    update task-6

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,10 +2,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]); // 1
-getFirstElement(['a', 'b', 'c']); // "a"
-getFirstElement([true, false, true]); // true
-
 const firstNumber = getFirstElement<number>([1, 2, 3]); // number
 const firstString = getFirstElement<string>(['a', 'b', 'c']); // string
 const firstBoolean = getFirstElement<boolean>([true, false, true]); // boolean
